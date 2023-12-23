93a519c08319ade8f15f1c5f596c5c35a60be234
Satish Tiwari <tiwarisatish985@gmail.com>
2023-12-23 12:49:57 +0530

    '....'

---
+++ routes/questionRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+const { allSubjectsName,searchBySubjectName,getAllQuestions, createQuestion, updateQuestion, deleteQuestion, getAquestion } = require('../controllers/questionsController');
+const { isAuthenticatedAdmin,authorizedAdminRoles } = require('../middleware/authentication');
+
+router.route('/questions/all-subjects').get(allSubjectsName);
+router.route('/questions/all-subjects/:subject').get(searchBySubjectName);
+router.route('/questions').get(getAllQuestions);
+router.route('/admin/questions/new').post(isAuthenticatedAdmin,authorizedAdminRoles("admin","writer"),createQuestion);
+router.route('/admin/questions/:id').put(isAuthenticatedAdmin,authorizedAdminRoles("admin","writer"),updateQuestion).delete(isAuthenticatedAdmin,authorizedAdminRoles("admin"),deleteQuestion);
+router.route('/questions/:id').get(getAquestion);
+
+module.exports = router;
